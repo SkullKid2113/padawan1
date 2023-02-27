3584525c5fe97da459965bc983160a726cc2fbd4
SkullKid2113 <125101202+SkullKid2113@users.noreply.github.com>
2023-02-27 17:53:34 -0600

    starting to look ok

--- file.js
+++ file.js
@@ -1,6 +1,20 @@
-var fs = require('fs');
-var fileBuffer = Buffer.from(file);
-var file = ('test.txt');
-fs.readFile(file,function(err,buffer){
-    console.log(buffer);
+import { open, close } from 'node:fs';
+
+open('myfile', 'r', (err, fd) => {
+  if (err) {
+    if (err.code === 'ENOENT') {
+      console.error('myfile does not exist');
+      return;
+    }
+
+    throw err;
+  }
+
+  try {
+    readMyData(fd);
+  } finally {
+    close(fd, (err) => {
+      if (err) throw err;
+    });
+  }
 });
